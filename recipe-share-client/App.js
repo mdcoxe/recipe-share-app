cd8fd26e5885b7031291074b8916920d00c80728
Michael Coxe <mcoxe@me.com>
2021-01-26 22:12:04 -0900

    Added in initial components I will need to bbuild app

--- recipe-share-client/App.js
+++ recipe-share-client/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>It's Alive!!!!!</Text>
+      <Text>hello</Text>
       <StatusBar style="auto" />
     </View>
   );
