88c5b8f3e34b5d7bb55849ca17a3a2a126b24ad2
Michael Coxe <mcoxe@me.com>
2021-01-26 23:10:46 -0900

    commit for nighgt

---
+++ recipe-share-client/components/Register/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Button, TextInput, StyleSheet } from 'react-native';
+
+const Register = () => {
+    
+
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default Register;
